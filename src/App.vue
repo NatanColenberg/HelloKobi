62ac30f01af8b48024f2cb31265c7ecfd4ee8c91
Natan Colenberg <natan.colenberg@gmail.com>
2019-07-08 01:36:02 +0300

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,7 +21,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <h1>Hey Kobi</h1>
+    <h3>You might be wondering what is the Change Healthcare stock price</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header{
+    padding: 20px;
+    margin: 20px;
+    border: 1px solid black;
+    border-radius: 20px;
+    background: linear-gradient(to bottom, #00b7ea 0%,#009ec3 100%);
+    
+  }
+  h1{
+    font-size: 60px;
+    color: rgb(238, 238, 238);
+    text-shadow: 3px 3px #000000;
+  }
+  h3{
+    font-size: 25px;
+    color: rgb(255, 255, 255);
+  }
+</style>
